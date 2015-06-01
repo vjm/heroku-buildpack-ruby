cdc905aaca3706c8d2e871b8f1d8884576d2bcfa
Vince Montalbano <vince.montalbano@gmail.com>
2015-06-01 01:32:05 -0500

    Update fetcher.rb

--- lib/language_pack/fetcher.rb
+++ lib/language_pack/fetcher.rb
@@ -12,7 +12,7 @@ module LanguagePack
     def initialize(host_url, stack = nil)
       @config   = load_config
       @host_url = fetch_cdn(host_url)
-      @host_url += File.basename(stack) if stack
+      # @host_url += File.basename(stack) if stack
     end
 
     def fetch(path)
